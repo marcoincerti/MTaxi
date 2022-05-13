a664f0de1ceb03de4be48455f4d58887ba2f12b1
marcoincerti <56672035+marcoincerti@users.noreply.github.com>
2022-05-13 11:13:09 +0200

    add CoordMTaxiList for manage mTaxi Coordinate

---
+++ src/main/java/RestServer/beans/CoordMTaxiList.java
@@ -0,0 +1,32 @@
+package RestServer.beans;
+
+import java.util.ArrayList;
+
+public class CoordMTaxiList {
+    private ArrayList<MTaxi> mTaxisList;
+    private int[] coordinates;
+
+    public CoordMTaxiList() {
+    }
+
+    public CoordMTaxiList(ArrayList<MTaxi> mTaxisList, int[] coordinates) {
+        this.mTaxisList = mTaxisList;
+        this.coordinates = coordinates;
+    }
+
+    public ArrayList<MTaxi> getmTaxisList() {
+        return mTaxisList;
+    }
+
+    public void setmTaxisList(ArrayList<MTaxi> mTaxisList) {
+        this.mTaxisList = mTaxisList;
+    }
+
+    public int[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(int[] coordinates) {
+        this.coordinates = coordinates;
+    }
+}
--- src/main/java/RestServer/beans/MTaxis.java
+++ src/main/java/RestServer/beans/MTaxis.java
@@ -13,7 +13,7 @@ import static java.lang.Math.abs;
 
 public class MTaxis {
 
-    @XmlRootElement(name = "mTaxis")
+    //@XmlRootElement(name = "mTaxis")
     private ArrayList<MTaxi> mTaxisList;
 
     private static MTaxis instance;
@@ -38,5 +38,33 @@ public class MTaxis {
         int x = abs(rd.nextInt()%10);
         int y = abs(rd.nextInt()%10);
         return new int[]{x, y};
+    }
+
+    public synchronized CoordMTaxiList add(MTaxi u){
+
+        for ( MTaxi t : this.getmTaxisList() ) {
+            if (u.getId() == t.getId())
+                return null;
+        }
+        this.mTaxisList.add(u);
+        return new CoordMTaxiList(getmTaxisList(), randomCoordinates());
+    }
+
+    public synchronized MTaxi getById(int id){
+        for ( MTaxi t : this.getmTaxisList() ) {
+            if ( id == t.getId())
+                return t;
+        }
+        return null;
+    }
+
+    public synchronized MTaxi deleteById(int id) {
+        for ( MTaxi t : this.mTaxisList ) {
+            if (t.getId() == id) {
+                this.mTaxisList.remove(t);
+                return t;
+            }
+        }
+        return null;
     }
 }
